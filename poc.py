e578fedc1851c39a02706f8ed5ee942dea77ac2a
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2024-03-02 13:28:30 -0500

    j

--- poc.py
+++ poc.py
@@ -1,7 +1,15 @@
 class Block: 
-    def __init__(self, course): 
+    def __init__(self, course, done): 
         self.course = course
+        self.done = done
 
+    def __init__(self,course): 
+        self.course = course
+        self.done = False
+        
+    def finish(self):
+        self.done = True
+    
 class Partition: 
     def __init__(self, containers):
         if type(containers) != 'list':
@@ -13,15 +21,16 @@ class Partition:
             
 
 class Container: 
-
     def __init__(self, children):
         self.children = children
         self.size = 0        
-    
-    def verify(self): 
-        if self.children:
-            return True
 
+    def verify(self): 
+        for child in self.children: 
+            if type(child) != "Block":
+                print("The child is a block ")
+            if type(child) != "Partition": 
+                pass
 
 
 
